b4ebdc3e54380e0b4daec3147b0e7a6c437d5b7e
Bed Prasad Pokharel <pokhrelnabin17@gmail.com>
2026-09-02 18:14:38 +0545

    fix: standardize formatting in index.js and socketHandlers.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
   cors: { origin: ["http://localhost:5173",
      "http://localhost:5000",
      "http://192.168.1.71:5173",
-     "http://192.168.137.1:5173/",
+     "http://192.168.137.1:5173",
     "https://vedmeet.netlify.app"] },
 });
 io.on("connection", (socket) => {
